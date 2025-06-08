98e921c3c317c92b344bc573ee78ce551e2e638f
Sufi6692 <sayedsufikhalandar@gmail.com>
2025-06-08 18:53:44 +0530

    Find the largest of the 3 numbers

---
+++ src/Largest.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Largest {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter number A : ");
+        int a = in.nextInt();
+        System.out.print("Enter number B : ");
+        int b = in.nextInt();
+        System.out.print("Enter number C : ");
+        int c = in.nextInt();
+
+        // Q : Find the largest of the 3 numbers
+        int max = a;
+        if(b > max){
+            max = b;
+        }
+        if(c > max){
+            max = c;
+        }
+
+        System.out.println("Your MAX number is : " + max);
+    }
+}
